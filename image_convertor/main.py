d9f3eb006370e48b659d392e1ddca6a9126fe5b3
JAKUB BROZ <Broz.Jk@gmail.com>
2022-03-02 12:45:07 +0100

    cv2 and image show

--- image_convertor/main.py
+++ image_convertor/main.py
@@ -1,12 +1,20 @@
-def main():
+import cv2
+import matplotlib.pyplot as plt
+
+
+def intro():
     print("""Welcome.
 This program will help you change your pictures, by 
 adjusting his colors and brightness !!!\n""")
 
     while True:
         try:
-            name_of_img = input("At first type the name of the image to the console:")
-            img = open(name_of_img)
+            image = input("At first type the name of the image to the console:")
+            cv2.imshow("Your picture", image)
+            cv2.waitKey(0)
+            cv2.destroyWindow("Your picture")
+            print("""In image manipulation you have these possibilities:
+                At first you have to decide, if you want to change color or brightness.""")
         except IOError:
             print("""Probably picked image, which is not in this directory or
              you could type incorrect way.
@@ -15,6 +23,12 @@ adjusting his colors and brightness !!!\n""")
         else:
             print("Your picture was picked and is prepared for changes.")
             break
+    return image
+def choices(img):
+
+
+def main():
+    img = intro()
 
 
 if __name__ == "__main__":
